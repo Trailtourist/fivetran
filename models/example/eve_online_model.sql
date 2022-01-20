22bbbc3b32b751c94a9742a1f241ee6fcbc4b285
Tobias <tobias.geipert@entual.de>
2022-01-20 18:53:39 +0100

    alias added

--- models/example/eve_online_model.sql
+++ models/example/eve_online_model.sql
@@ -6,8 +6,8 @@
 select 
   SYSTEM_ID
   , TYPE_ID
-  , sum(volume_remain)
-  , avg(price)
+  , sum(volume_remain) as sum_vol_remain
+  , avg(price) as avg_price
 from GOOGLE_CLOUD_FUNCTION.EVE_ORDERS
 group by
   SYSTEM_ID
